84fb242c69bd398fa977a39c5a9039d5a9736a93
mjohra <mehjabin.johra00@gmail.com>
2021-11-15 15:37:14 +0600

    first commit

---
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import {
+  Alert,
+  Button,
+  Col,
+  Container,
+  FloatingLabel,
+  Form,
+  Row,
+} from "react-bootstrap";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import admin from "../../../images/admin.png";
+import "./MakeAdmin.css";
+
+const MakeAdmin = () => {
+  const [email, setEmail] = useState("");
+  const [success, setSuccess] = useState(false);
+  const handleOnBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleAdminSubmit = (e) => {
+    const user = { email };
+    fetch("https://nameless-headland-75190.herokuapp.com/users/admin", {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          setSuccess(true);
+        }
+      });
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <Container className="text center add-service pt-5">
+        <Row>
+          <Col className="text center p-lg-5" lg={6} md={6} sm={12}>
+            <h1 className="text-center text-title text-gray">Make Admin</h1>
+            <Form onSubmit={handleAdminSubmit}>
+              <Form.Control
+                className="w-100"
+                type="email"
+                onBlur={handleOnBlur}
+                placeholder="Enter email"
+              />
+              <Button className="btn-buy" type="submit">
+                Submit
+              </Button>
+            </Form>
+            {success && (
+              <Alert variant="success">Admin made successfully</Alert>
+            )}
+          </Col>
+          <Col lg={6} md={6} sm={12}>
+            <img className="img-fluid" src={admin} alt="" />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default MakeAdmin;
---
+++ src/Pages/Home/About/About.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import about from "../../../images/about.jpg";
+
+const About = () => {
+  return (
+    <div id="about">
+      <div className="container-fluid mt-5 mb-5">
+        <h1 className="text-center mt-5 text-title">About Us</h1>
+        <div className="row">
+          <div className="col-md-6 col-12 mt-lg-5 pt-lg-5">
+            <h3 className="text-center mt-5 title-header">Establishment</h3>
+            <p className="text-left px-5 py-3">
+              More than a million of the world's most successful brands trust
+              Shopsy to sell, ship and process payments anywhere. Shopsy has
+              grown from 5 people in a coffee shop to over 5,000 across the
+              globe. With over 1,700,000 businesses powered by Shopsy, we care
+              deeply about the work we do. We’re constant learners who thrive on
+              change and seek to have an impact in everything we do.
+            </p>
+            <div className="text-center">
+            <Link className="text-center" to="/explore">
+              <button className="btn-buy text-center px-4 py-2">Shop Now</button>
+            </Link>
+            </div>
+            {/* <Link to={'/detailabout'}><button className="view-more text-center mt-1">Read More</button></Link> */}
+          </div>
+          <div className="col-md-6 col-12">
+            <img className="w-100" src={about} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
